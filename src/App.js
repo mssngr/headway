d60ae219a2611f30c42dcaac3b2856d1bb2b38e7
mssngr <gabriel.konkle@gmail.com>
2018-05-28 20:12:11 -0600

    Major updates

--- src/App.js
+++ src/App.js
@@ -1,21 +1,84 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import styled from 'styled-components'
+import MuiDrawer from '@material-ui/core/Drawer'
+import Hidden from '@material-ui/core/Hidden'
+import List from '@material-ui/core/List'
+import Divider from '@material-ui/core/Divider'
+import 'normalize.css'
 
-class App extends Component {
+import Home from 'screens/Home'
+
+import { styledMui } from 'utils'
+import 'App.css'
+
+/* STYLES */
+const AppContainer = styled.div`
+  position: relative;
+  flex: 1;
+  display: flex;
+  width: 100%;
+  overflow: hidden;
+`
+
+const AppDrawer = styledMui(MuiDrawer)(
+  theme => ({
+    paper: {
+      width: 240,
+      [theme.breakpoints.up('md')]: {
+        position: 'relative',
+      },
+    },
+  }),
+  { name: 'AppDrawer' }
+)
+
+/* PRESENTATION */
+export default class App extends React.Component {
+  state = {
+    isMobileOpen: false,
+  }
+
+  toggleDrawer = () => {
+    this.setState({ isMobileOpen: !this.state.isMobileOpen })
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+    const drawer = (
+      <div>
+        <List>List</List>
+        <Divider />
+        <List>Other List</List>
       </div>
-    );
+    )
+
+    return (
+      <AppContainer>
+        <Hidden mdUp>
+          <button onClick={this.toggleDrawer}>Click Me</button>
+          <AppDrawer
+            open={this.state.isMobileOpen}
+            onClose={this.toggleDrawer}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            <div
+              tabIndex={0}
+              role="button"
+              onClick={this.toggleDrawer}
+              onKeyDown={this.toggleDrawer}
+            >
+              {drawer}
+            </div>
+          </AppDrawer>
+        </Hidden>
+        <Hidden smDown implementation="css">
+          <AppDrawer variant="permanent" open>
+            {drawer}
+          </AppDrawer>
+        </Hidden>
+        <Home />
+      </AppContainer>
+    )
   }
 }
-
-export default App;
---
+++ src/utils/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { withStyles } from '@material-ui/core/styles'
+
+export const styledMui = Component => {
+  return (style, options) => {
+    const StyledComponent = props => <Component {...props} />
+    const styles =
+      typeof style === 'function'
+        ? theme => ({ ...style(theme) })
+        : { ...style }
+    return withStyles(styles, options)(StyledComponent)
+  }
+}
