d0db21be30a23f811a6c9a83781dbe342174d2e3
mssngr <gabriel.konkle@gmail.com>
2018-11-02 08:07:45 -0600

    Update to create react app 2

--- src/utils/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { withStyles } from '@material-ui/core/styles'
-
-export const styledMui = Component => (style, options) => {
-  const StyledComponent = props => <Component {...props} />
-  const styles =
-    typeof style === 'function' ? theme => ({ ...style(theme) }) : { ...style }
-  return withStyles(styles, options)(StyledComponent)
-}
